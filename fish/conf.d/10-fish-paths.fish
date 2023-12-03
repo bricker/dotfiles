3e4f6779a6dc548ab22bf867676ad61350def4a2
Bryan Ricker <git@bryanricker.com>
2023-12-03 14:43:05 -0800

    Sun Dec  3 02:43:05 PM PST 2023

--- fish/conf.d/10-fish-paths.fish
+++ fish/conf.d/10-fish-paths.fish
@@ -1,3 +1,4 @@
 set --prepend fish_function_path $DOTFILES_HOME/fish/functions
 set --prepend fish_complete_path $DOTFILES_HOME/fish/completions
+fish_add_path -g $HOME/.local/bin
 fish_add_path -g $DOTFILES_HOME/bin
---
+++ fish/conf.d/10-rust.fish
@@ -0,0 +1,3 @@
+set -gx CARGO_HOME $XDG_CONFIG_HOME/cargo
+set -gx RUSTUP_HOME $XDG_CONFIG_HOME/rustup
+fish_add_path -g $XDG_CONFIG_HOME/cargo/bin
