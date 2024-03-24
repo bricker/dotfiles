6468bcde9baadc56880efd9c2888bc2785456064
Bryan Ricker <git@bryanricker.com>
2024-03-23 21:22:13 -0700

    Sat Mar 23 09:22:13 PM PDT 2024

--- fish/conf.d/10-k8s.fish
+++ fish/conf.d/10-k8s.fish
@@ -1,11 +1,13 @@
-if command -v kubectl > /dev/null
-  kubectl completion fish | source
-end
+# These are SLOW.
 
-if command -v minikube > /dev/null
-  minikube completion fish | source
-end
+# if command -v kubectl > /dev/null
+#   kubectl completion fish | source
+# end
 
-if command -v helm > /dev/null
-  helm completion fish | source
-end
+# if command -v minikube > /dev/null
+#   minikube completion fish | source
+# end
+
+# if command -v helm > /dev/null
+#   helm completion fish | source
+# end
