e9ac47c390fd910edf358776d411c46f2fa8b6b3
Bryan Ricker <git@bryanricker.com>
2026-01-26 15:45:53 -0800

    Mon Jan 26 15:45:53 PST 2026

--- fish/conf.d/10-homebrew.fish
+++ fish/conf.d/10-homebrew.fish
@@ -1,7 +1,7 @@
 set -gx HOMEBREW_NO_ANALYTICS 1
 
-set -l brewcmd "/home/linuxbrew/.linuxbrew/bin/brew"
-if test -x "$brewcmd"
+set -l brewcmd "$(which brew)"
+if test -n "$brewcmd"
     set -gx HOMEBREW_ROOT ($brewcmd --prefix)
     fish_add_path -g $HOMEBREW_ROOT/bin
 end
